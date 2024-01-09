0c8203e9f0d5a3c87b2625fbc9ee117ca1f203b3
Mohamad Farhoud <Mohamadfarhoud@hotmail.com>
2024-01-09 22:12:22 +0100

    Add ChecklistItem model

---
+++ Models/ChecklistItem.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace MauiTripPlanner.Models
+{
+	public class ChecklistItem : INotifyPropertyChanged
+	{
+		[PrimaryKey, AutoIncrement] 
+		public int Id { get; set; }
+		public string Name { get; set; }
+		private bool isChecked { get; set; } = false;
+		public bool IsChecked
+		{
+			get => isChecked;
+			set
+			{
+				isChecked = value;
+				OnPropertyChanged();
+			}
+		}
+
+		#region INotifyPropertyChanged
+		public event PropertyChangedEventHandler PropertyChanged;
+		protected void OnPropertyChanged([CallerMemberName] string propertyName = "")
+		{
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+		}
+		public void RaisePropertyChanged(params string[] properties)
+		{
+			foreach (var propertyName in properties)
+			{
+				PropertyChanged?.Invoke(this, new
+				PropertyChangedEventArgs(propertyName));
+			}
+		}
+		#endregion
+
+	}
+}
